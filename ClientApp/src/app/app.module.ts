418ceb3931040b1b3f24d83d76ce7ead0858ed33
kowi1 <dolukowi@gmail.com>
2020-12-29 06:35:56 +0100

    Directory Clean-up

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,18 +6,12 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     FileUploadComponent
   ],
   imports: [
@@ -26,9 +20,7 @@ import { FileUploadComponent } from './file-upload/file-upload.component';
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: FileUploadComponent, pathMatch: 'full' },
       { path: 'file-upload', component: FileUploadComponent },
     ])
   ],
